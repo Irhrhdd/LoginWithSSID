eb54af8b6fde331a0cdca50a20ad0c6058b06cac
Roy Sherman <92811592+Irhrhdd@users.noreply.github.com>
2025-05-23 20:30:15 -0700

    Update BedWarsStatsTab.java

--- src/main/java/com/example/bedwarsstatstab/BedWarsStatsTab.java
+++ src/main/java/com/example/bedwarsstatstab/BedWarsStatsTab.java
@@ -1,7 +1,11 @@
 package com.example.bedwarsstatstab;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiIngame;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+import java.lang.reflect.Field;
 
 @Mod(modid = BedWarsStatsTab.MODID, name = BedWarsStatsTab.NAME, version = BedWarsStatsTab.VERSION)
 public class BedWarsStatsTab {
@@ -11,7 +15,17 @@ public class BedWarsStatsTab {
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
-        new TabOverlayHook();
-        CommandSetApiKey.register();
+        Minecraft mc = Minecraft.getMinecraft();
+        GuiIngame guiIngame = mc.ingameGUI;
+
+        try {
+            Field tabListField = GuiIngame.class.getDeclaredField("tabList");
+            tabListField.setAccessible(true);
+            tabListField.set(guiIngame, new CustomTabOverlay(mc, guiIngame));
+            System.out.println("[BedWarsStatsTab] CustomTabOverlay installed!");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println("[BedWarsStatsTab] Failed to install CustomTabOverlay.");
+        }
     }
 }
