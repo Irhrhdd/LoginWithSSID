871a1a4a463ad9fe0f88116a7c050fcdd0900540
Roy Sherman <92811592+Irhrhdd@users.noreply.github.com>
2025-05-23 18:45:13 -0700

    Update CustomTabOverlay.java

--- src/main/java/com/example/bedwarsstatstab/CustomTabOverlay.java
+++ src/main/java/com/example/bedwarsstatstab/CustomTabOverlay.java
@@ -7,28 +7,15 @@ import net.minecraft.client.network.NetworkPlayerInfo;
 import net.minecraft.scoreboard.ScorePlayerTeam;
 
 public class CustomTabOverlay extends GuiPlayerTabOverlay {
-
     public CustomTabOverlay(Minecraft mc, GuiIngame guiIngame) {
         super(mc, guiIngame);
     }
 
     @Override
-    public String getPlayerName(NetworkPlayerInfo info) {
-        // DEBUG: Check if this method is being called
-        String originalName = info.getGameProfile().getName();
-        String debugPrefix = "§7[DBG] ";
-
-        // Attempt to retrieve stats from the cache
-        String stats = StatsCache.get(info.getGameProfile().getId());
-
-        if (stats.isEmpty()) {
-            return debugPrefix + originalName; // Show debug tag if no stats found
-        }
-
-        String fullName = stats + " " + originalName;
+    protected String getPlayerName(NetworkPlayerInfo info) {
+        String name = super.getPlayerName(info);
         ScorePlayerTeam team = info.getPlayerTeam();
-
-        // Return formatted name with team styling if applicable
-        return team != null ? team.formatString(fullName) : fullName;
+        String formatted = (team != null ? team.formatString("[DBG] " + name) : "[DBG] " + name);
+        return formatted;
     }
 }
