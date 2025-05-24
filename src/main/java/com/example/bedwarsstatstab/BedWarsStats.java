340e2f0844b01b0b5672a069d14481ac2d615d72
Roy Sherman <92811592+Irhrhdd@users.noreply.github.com>
2025-05-23 19:53:44 -0700

    Create BedWarsStats.java

---
+++ src/main/java/com/example/bedwarsstatstab/BedWarsStats.java
@@ -0,0 +1,11 @@
+package com.example.bedwarsstatstab;
+
+public class BedWarsStats {
+    public final int star;
+    public final double fkdr;
+
+    public BedWarsStats(int star, double fkdr) {
+        this.star = star;
+        this.fkdr = fkdr;
+    }
+}
