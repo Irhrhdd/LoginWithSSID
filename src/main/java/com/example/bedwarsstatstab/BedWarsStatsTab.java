af8f3d91fea948f5669f979e88e42d29074f0c21
Roy Sherman <92811592+Irhrhdd@users.noreply.github.com>
2025-05-23 19:48:23 -0700

    Update BedWarsStatsTab.java

--- src/main/java/com/example/bedwarsstatstab/BedWarsStatsTab.java
+++ src/main/java/com/example/bedwarsstatstab/BedWarsStatsTab.java
@@ -1,12 +1,8 @@
 package com.example.bedwarsstatstab;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.GuiIngame;
-import net.minecraft.client.gui.GuiPlayerTabOverlay;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-
-import java.lang.reflect.Field;
+import net.minecraftforge.common.MinecraftForge;
 
 @Mod(modid = BedWarsStatsTab.MODID, name = BedWarsStatsTab.NAME, version = BedWarsStatsTab.VERSION)
 public class BedWarsStatsTab {
@@ -16,26 +12,10 @@ public class BedWarsStatsTab {
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
-        try {
-            Minecraft mc = Minecraft.getMinecraft();
-            GuiIngame ingameGUI = mc.ingameGUI;
+        // Register the event handler for rendering the custom tab info
+        MinecraftForge.EVENT_BUS.register(new TabOverlayHook());
 
-            // Loop through fields to find the GuiPlayerTabOverlay
-            for (Field field : GuiIngame.class.getDeclaredFields()) {
-                if (GuiPlayerTabOverlay.class.isAssignableFrom(field.getType())) {
-                    field.setAccessible(true);
-                    field.set(ingameGUI, new CustomTabOverlay(mc, ingameGUI));
-                    GuiPlayerTabOverlay test = (GuiPlayerTabOverlay) field.get(ingameGUI);
-                    System.out.println("[BedWarsStatsTab] Successfully injected CustomTabOverlay into: " + field.getName());
-                    System.out.println("[BedWarsStatsTab] Overlay class now: " + test.getClass().getName());
-                    break;
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        // Register command
+        // Register the command for setting the API key
         CommandSetApiKey.register();
     }
 }
