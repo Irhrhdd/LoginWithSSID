512969066728e30a3a7f93b4d316f20322414f52
Roy Sherman <92811592+Irhrhdd@users.noreply.github.com>
2025-05-23 17:49:37 -0700

    Update CommandSetApiKey.java

--- src/main/java/com/example/bedwarsstatstab/CommandSetApiKey.java
+++ src/main/java/com/example/bedwarsstatstab/CommandSetApiKey.java
@@ -1,8 +1,11 @@
 package com.example.bedwarsstatstab;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.util.ChatComponentText;
+import net.minecraftforge.client.ClientCommandHandler;
 
 public class CommandSetApiKey extends CommandBase {
 
@@ -21,7 +24,9 @@ public class CommandSetApiKey extends CommandBase {
         if (args.length == 2 && args[0].equalsIgnoreCase("setkey")) {
             String key = args[1];
             HypixelAPI.setApiKey(key);
-            sender.addChatMessage(new ChatComponentText("§a[BedWarsStatsTab] API key set!"));
+            if (sender instanceof EntityPlayerSP) {
+                ((EntityPlayerSP) sender).addChatMessage(new ChatComponentText("§a[BedWarsStatsTab] API key set!"));
+            }
         } else {
             sender.addChatMessage(new ChatComponentText("§cUsage: /bwstats setkey <api-key>"));
         }
@@ -30,5 +35,9 @@ public class CommandSetApiKey extends CommandBase {
     @Override
     public int getRequiredPermissionLevel() {
         return 0;
+    }
+
+    public static void register() {
+        ClientCommandHandler.instance.registerCommand(new CommandSetApiKey());
     }
 }
