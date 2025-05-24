0930b7178187465a0452343152ac54146b134284
Roy Sherman <92811592+Irhrhdd@users.noreply.github.com>
2025-05-23 18:20:02 -0700

    Update BedWarsStatsTab.java

--- src/main/java/com/example/bedwarsstatstab/BedWarsStatsTab.java
+++ src/main/java/com/example/bedwarsstatstab/BedWarsStatsTab.java
@@ -1,9 +1,13 @@
 package com.example.bedwarsstatstab;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiIngame;
+import net.minecraft.client.gui.GuiPlayerTabOverlay;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+import java.lang.reflect.Field;
 
 @Mod(modid = BedWarsStatsTab.MODID, name = BedWarsStatsTab.NAME, version = BedWarsStatsTab.VERSION)
 public class BedWarsStatsTab {
@@ -13,9 +17,18 @@ public class BedWarsStatsTab {
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
-        Minecraft mc = Minecraft.getMinecraft();
-        mc.ingameGUI.tabList = new CustomTabOverlay(mc, mc.ingameGUI);
+        replaceTabOverlay(Minecraft.getMinecraft());
         MinecraftForge.EVENT_BUS.register(new TabOverlayHook());
         CommandSetApiKey.register();
+    }
+
+    private void replaceTabOverlay(Minecraft mc) {
+        try {
+            Field tabField = GuiIngame.class.getDeclaredField("field_175196_v"); // tab list field (obfuscated)
+            tabField.setAccessible(true);
+            tabField.set(mc.ingameGUI, new CustomTabOverlay(mc, mc.ingameGUI));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
