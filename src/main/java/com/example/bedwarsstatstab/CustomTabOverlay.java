dd1328cc210ed3801fac91db7a8034b311bdfeee
Roy Sherman <92811592+Irhrhdd@users.noreply.github.com>
2025-05-23 18:17:13 -0700

    Create CustomTabOverlay.java

---
+++ src/main/java/com/example/bedwarsstatstab/CustomTabOverlay.java
@@ -0,0 +1,24 @@
+package com.example.bedwarsstatstab;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiIngame;
+import net.minecraft.client.gui.GuiPlayerTabOverlay;
+import net.minecraft.client.network.NetworkPlayerInfo;
+import net.minecraft.scoreboard.ScorePlayerTeam;
+
+public class CustomTabOverlay extends GuiPlayerTabOverlay {
+
+    public CustomTabOverlay(Minecraft mc, GuiIngame guiIngame) {
+        super(mc, guiIngame);
+    }
+
+    @Override
+    protected String getPlayerName(NetworkPlayerInfo info) {
+        String name = info.getGameProfile().getName();
+        String stats = StatsCache.get(info.getGameProfile().getId());
+        String full = (stats.isEmpty() ? name : stats + " " + name);
+
+        ScorePlayerTeam team = info.getPlayerTeam();
+        return team != null ? team.formatString(full) : full;
+    }
+}
